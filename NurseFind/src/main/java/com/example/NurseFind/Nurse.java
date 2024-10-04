bb170b1b26d8971285cdace74c2625e8c7980d92
dafne <dafne@Dafne>
2024-10-04 15:22:25 +0200

    Update NurseFind

--- NurseFind/src/main/java/com/example/NurseFind/Nurse.java
+++ NurseFind/src/main/java/com/example/NurseFind/Nurse.java
@@ -1,15 +1,48 @@
 package com.example.NurseFind;
 
-	public class Nurse extends NurseController{
-		private String nombre;
-		
-		public Nurse (String nombre) {
-			this.nombre=nombre;
-		}
-		public String getNombre() {
-			return nombre;
-		}
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
+public class Nurse {
+	private String nombre;
+	private String color;
+	private int edad;
+	private String profesionalidad;
+
+	public Nurse(String nombre, String color,int edad, String profesionalidad) {
+		this.nombre = nombre;
+		this.color = color;
+		this.edad=edad;
+		this.profesionalidad=profesionalidad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getSector() {
+		return profesionalidad;
+	}
+
+	public void setSector(String profesionalidad) {
+		this.profesionalidad = profesionalidad;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 }
